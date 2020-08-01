7d93d7682d1fdfc6958a7a3e0eda7d0a214466cf
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2020-08-01 10:47:44 +0900

    add provider skelton

---
+++ concourse/provider.go
@@ -0,0 +1,32 @@
+package concourse
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// Provider serves terraform provider
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"username": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"password": {
+				Type:      schema.TypeString,
+				Required:  true,
+				Sensitive: true,
+			},
+		},
+		ResourcesMap:         map[string]*schema.Resource{},
+		DataSourcesMap:       map[string]*schema.Resource{},
+		ConfigureContextFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	return nil, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cappyzawa/terraform-provider-concourse
+
+go 1.14
+
+require github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.0
