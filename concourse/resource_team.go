03146ed5190262dc4ba60410101580841e97a5cc
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2020-08-02 23:19:47 +0900

    fix team resource
    member, viewer and pipeline-operator can be enabled to set.

--- concourse/resource_team.go
+++ concourse/resource_team.go
@@ -67,34 +67,8 @@ func resourceTeam() *schema.Resource {
 
 func resourceTeamCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-
 	client := m.(cc.Client)
-	teamName := d.Get("name").(string)
-	ownerGroups := ifs2strs(d.Get("owner_groups").([]interface{}))
-	ownerUsers := ifs2strs(d.Get("owner_users").([]interface{}))
-	team := atc.Team{
-		Name: teamName,
-		Auth: atc.TeamAuth{
-			"owner": {
-				"groups": ownerGroups,
-				"users":  ownerUsers,
-			},
-		},
-	}
-	createdTeam, _, _, err := client.Team(teamName).CreateOrUpdate(team)
-	if err != nil {
-		diags = append(diags, diag.Diagnostic{
-			Severity: diag.Error,
-			Summary:  "failed to create team",
-			Detail:   err.Error(),
-		})
-		return diags
-	}
-	d.SetId(strconv.Itoa(createdTeam.ID))
-	t := flattenTeamAuthData(createdTeam.Auth)
-	for k, v := range t {
-		d.Set(k, v)
-	}
+	diags = append(diags, createOrUpdateTeamFromData(d, client)...)
 	return diags
 }
 
@@ -134,31 +108,7 @@ func resourceTeamUpdate(ctx context.Context, d *schema.ResourceData, m interface
 			return diags
 		}
 	}
-	teamName := d.Get("name").(string)
-	ownerGroups := ifs2strs(d.Get("owner_groups").([]interface{}))
-	ownerUsers := ifs2strs(d.Get("owner_users").([]interface{}))
-	team := atc.Team{
-		Name: teamName,
-		Auth: atc.TeamAuth{
-			"owner": {
-				"groups": ownerGroups,
-				"users":  ownerUsers,
-			},
-		},
-	}
-	createdTeam, _, _, err := client.Team(teamName).CreateOrUpdate(team)
-	if err != nil {
-		diags = append(diags, diag.Diagnostic{
-			Severity: diag.Error,
-			Summary:  "failed to create team",
-		})
-		return diags
-	}
-	d.SetId(strconv.Itoa(createdTeam.ID))
-	t := flattenTeamAuthData(createdTeam.Auth)
-	for k, v := range t {
-		d.Set(k, v)
-	}
+	diags = append(diags, createOrUpdateTeamFromData(d, client)...)
 	return diags
 }
 
@@ -184,4 +134,52 @@ func ifs2strs(ifs []interface{}) []string {
 		strs = append(strs, f.(string))
 	}
 	return strs
+}
+
+func createOrUpdateTeamFromData(d *schema.ResourceData, client cc.Client) diag.Diagnostics {
+	teamName := d.Get("name").(string)
+	ownerGroups := ifs2strs(d.Get("owner_groups").([]interface{}))
+	ownerUsers := ifs2strs(d.Get("owner_users").([]interface{}))
+	memberGroups := ifs2strs(d.Get("member_groups").([]interface{}))
+	memberUsers := ifs2strs(d.Get("member_users").([]interface{}))
+	viewerGroups := ifs2strs(d.Get("viewer_groups").([]interface{}))
+	viewerUsers := ifs2strs(d.Get("viewer_users").([]interface{}))
+	pipelineOperatorGroups := ifs2strs(d.Get("pipeline_operator_groups").([]interface{}))
+	pipelineOperatorUsers := ifs2strs(d.Get("pipeline_operator_users").([]interface{}))
+	team := atc.Team{
+		Name: teamName,
+		Auth: atc.TeamAuth{
+			"owner": {
+				"groups": ownerGroups,
+				"users":  ownerUsers,
+			},
+			"member": {
+				"groups": memberGroups,
+				"users":  memberUsers,
+			},
+			"viewer": {
+				"groups": viewerGroups,
+				"users":  viewerUsers,
+			},
+			"pipeline-operator": {
+				"groups": pipelineOperatorGroups,
+				"users":  pipelineOperatorUsers,
+			},
+		},
+	}
+	updatedTeam, _, _, err := client.Team(teamName).CreateOrUpdate(team)
+	var diags diag.Diagnostics
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "failed to create or update team",
+		})
+		return diags
+	}
+	d.SetId(strconv.Itoa(updatedTeam.ID))
+	t := flattenTeamAuthData(updatedTeam.Auth)
+	for k, v := range t {
+		d.Set(k, v)
+	}
+	return diags
 }
